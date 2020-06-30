8938cb689f4737c28c5373244145f089ecdefd78
mf76 <pv.mszk@gmail.com>
2020-06-30 12:21:59 +0900

    ファイル作成

---
+++ indx.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>アウル経済の請求書買い取り</title>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+</body>
+</html>
