258e6a347f44c003685a27206731d9b978bfda97
mf76 <pv.mszk@gmail.com>
2020-06-30 17:39:28 +0900

    コーディング開始

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>アウル経済の請求書買い取り</title>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <section id="top">
+    <div class="widthKeeper">
+      <img src="img/top__img01.svg" class="img01" alt="本当に安心できる資金調達なら">
+      <img src="img/top__img02.svg" class="img02" alt="アウル経済の請求書買取り">
+      <img src="img/top__img03.svg" class="img03" alt="業界最安水準手数料">
+      <button class="button"><img src="img/btn__top.svg" alt="無料相談はこちら"></button>
+    </div>
+  </section>
+  <section id="contact">
+    <div class="widthKeeper">
+      <h2><img src="img/contact__img_title.svg" alt="資金調達が可能かすぐにわかります"></h2>
+      <div class="contactBox flex">
+        <div class="left">
+          <button><img src="img/btn__check.svg" alt=""></button>
+        </div>
+        <div class="right">
+          <button><img src="img/contact__img_dial.svg" alt=""></button>
+          <button><img src="img/contact__img_line.svg" alt=""></button>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
