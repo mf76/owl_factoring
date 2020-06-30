29f80997b6de589f4a34de978b62af606a84baaf
mf76 <pv.mszk@gmail.com>
2020-06-30 19:57:45 +0900

    cautionのCSSまで

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>アウル経済の請求書買い取り</title>
   <link rel="stylesheet" href="css/main.css">
 </head>
+
 <body>
   <section id="top">
     <div class="widthKeeper">
@@ -27,15 +29,30 @@
           <h3>お問い合わせ</h3>
           <div class="flex">
             <button><img src="img/contact__img_dial.svg" alt=""></button>
-            <button><img src="img/contact__img_line.svg" alt=""></button>
+            <button><a href="https://page.line.me/065gwzlz"><img src="img/contact__img_line.svg" alt=""></a></button>
           </div>
         </div>
       </div>
     </div>
   </section>
   <section id="caution">
-    <img src="img/caution__img_border.svg" alt="">
-    <img src="img/caution__img_border.svg" alt="">
+    <img src="img/caution__img_border.svg" alt="危険ボーダー">
+    <div class="widthKeeper">
+      <div class="flex">
+        <div><img src="img/caution__img_news.jpg" alt="新聞見出し"></div>
+        <div class="right">
+          <img src="img/caution__img_triangle.svg" alt="">
+          <img src="img/caution__img_title.svg" alt="">
+          <p>
+            <a href="https://www.fsa.go.jp/ordinary/chuui/kinyu_chuui2.html" target="_blank">https://www.fsa.go.jp/ordinary/chuui/kinyu_chuui2.html</a>（金融庁サイト）<br><br>
+            最近、「給与ファクタリング」を謳ったヤミ金融などの詐欺事件が増えています。また法人相手でも悪質なファクタリング業者も存在するようです。<br>
+            悪質ファクタリングは個人情報を騙し取られたり違法な手数料を取られる犯罪ですが、売掛金の買取を行うアウル経済の請求書買取はこれらとは全く異なるサービスです。金銭消費貸借契約ではなく売買契約になりますので、安心してご利用いただけます。
+          </p>
+        </div>
+      </div>
+    </div>
+    <img src="img/caution__img_border.svg" class="flip-horizontal" alt="危険ボーダー">
   </section>
 </body>
+
 </html>
