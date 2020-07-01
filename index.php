48ffc78412af283b240801210f2a35e44faa587e
mf76 <pv.mszk@gmail.com>
2020-07-01 18:13:05 +0900

    フォントの調整済み

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>アウル経済の請求書買い取り</title>
   <link rel="stylesheet" href="css/main.css">
-  <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p" rel="stylesheet">
   <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
 </head>
 
