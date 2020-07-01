ae54962f01f271108649e4f68162c07cb2850ebd
mf76 <pv.mszk@gmail.com>
2020-07-01 13:51:17 +0900

    動き以外だいたいマークアップ

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>アウル経済の請求書買い取り</title>
   <link rel="stylesheet" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p" rel="stylesheet">
 </head>
 
 <body>
@@ -44,7 +45,7 @@
           <img src="img/caution__img_triangle.svg" alt="">
           <img src="img/caution__img_title.svg" alt="">
           <p>
-            <a href="https://www.fsa.go.jp/ordinary/chuui/kinyu_chuui2.html" target="_blank">https://www.fsa.go.jp/ordinary/chuui/kinyu_chuui2.html</a>（金融庁サイト）<br><br>
+            <a href="https://www.fsa.go.jp/ordinary/chuui/kinyu_chuui2.html" target="_blank">https://www.fsa.go.jp/ordinary/chuui/kinyu_chuui2.html<br>（金融庁サイト）</a><br><br>
             最近、「給与ファクタリング」を謳ったヤミ金融などの詐欺事件が増えています。また法人相手でも悪質なファクタリング業者も存在するようです。<br>
             悪質ファクタリングは個人情報を騙し取られたり違法な手数料を取られる犯罪ですが、売掛金の買取を行うアウル経済の請求書買取はこれらとは全く異なるサービスです。金銭消費貸借契約ではなく売買契約になりますので、安心してご利用いただけます。
           </p>
@@ -53,6 +54,255 @@
     </div>
     <img src="img/caution__img_border.svg" class="flip-horizontal" alt="危険ボーダー">
   </section>
+  <section id="why">
+    <div id="cantChoose">
+      <div class="widthKeeper">
+        <img src="img/why__img_cantChoose.svg" alt="業者が多すぎて選べない！">
+      </div>
+      <img src="img/why__img_arrow.svg" class="posA" alt="">
+    </div>
+    <div class="greenTitle">
+      <div class="widthKeeper posR">
+        <div class="transparentGreen">
+        </div>
+        <h2 class="greenBack">アウル経済の請求書買取りを<br>自信を持っておすすめします！</h2>
+      </div>
+    </div>
+    <div>
+      <div id="whyFlex" class="widthKeeper flex">
+        <img src="img/why__img_reason01.svg" alt="①最短2時間">
+        <img src="img/why__img_reason02.svg" alt="②手数料1％〜">
+        <img src="img/why__img_reason03.svg" alt="③オンラインで完結も可能">
+        <img src="img/why__img_reason04.svg" alt="④コンサル会社の運営だから">
+      </div>
+    </div>
+    <div class="paleGreenBg">
+      <div class="widthKeeper flex">
+        <div class="left">
+          <p>もちろんその他にも…</p>
+          <ul>
+            <li>・信用情報に傷がつかない</li>
+            <li>・完全買取で売掛先の倒産リスクもクリア</li>
+            <li>・秘密厳守！担保や保証人は不要</li>
+          </ul>
+          <p>…など、請求書買取サービスを安心して<br>
+            ご利用頂ける環境をご用意しております。</p>
+        </div>
+        <div class="right">
+          <img src="img/why__img_sign.jpg" alt="サインする男性">
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="flow">
+    <div class="widthKeeper">
+      <h2 class="greenH2">ご利用の流れ</h2>
+      <p>お申込み後、入金までの流れをご紹介します。</p>
+      <div class="flex">
+        <div><img src="img/flow__img02.svg" alt=""></div>
+        <div><img src="img/flow__img01.svg" alt=""></div>
+        <div><img src="img/flow__img03.svg" alt=""></div>
+        <div><img src="img/flow__img04.svg" alt=""></div>
+      </div>
+      <p>ご利用に際して、ご不明点やご要望があれば遠慮なくお申し付けください。</p>
+    </div>
+  </section>
+  <section id="voice">
+    <div class="widthKeeper">
+      <h2 class="greenH2">ご利用いただいたお客様の声</h2>
+    </div>
+  </section>
+  <section id="staff" class="paleGreenBg">
+    <div class="widthKeeper">
+      <h2 class="greenH2">スタッフ紹介</h2>
+      <div class="flex staffBox">
+        <h3>2年目／元金融コンサル</h3>
+        <div class="staffPic"><img src="img/staff_03.jpg" alt=""></div>
+        <div class="staffCap">
+          <h4>金融コンサルで培った経験を活かして、<br>お客様の資金繰り改善に尽力します！</h4>
+          <p>前職は個人のお客様を対象にした金融コンサルタントをしていました。お客様の大切な資産を、お客様の希望される方針に寄り添って運用をお手伝いさせていただく仕事です。<br>
+            その後ファクタリングを事業とするアウル経済に転職しましたが、日本では資金調達の選択肢が豊富にあることがあまり知られていません。ファクタリングは欧米では至ってスタンダードな資金調達方法です。大切なお客様のビジネスを止めない為に、資金調達を含めた総合的な経営サポートをさせていただければと思っています。</p>
+        </div>
+      </div>
+      <div class="flex staffBox">
+        <h3>4年目／起業経験あり</h3>
+        <div class="staffPic"><img src="img/staff_06.jpg" alt=""></div>
+        <div class="staffCap">
+          <h4>失敗を恐れず行動する中小経営者の方を<br>
+            サポートさせていただきたいです！
+          </h4>
+          <p>フリーランスで起業支援コンサルタントをしていました。多くのスモールビジネスの立ち上げに関わらせていただき、多くのお客様の資金調達をサポートさせていただいた経験があります。<br>
+            その上で感じていたのが、資金調達に関して経営者の方は様々な選択肢をあまりご存知ないということです。助成金や銀行融資といった方法しか知らず、資金調達が上手くいかず、ビジネスを大きくするチャンスを逃している状況を見ているともったいないなと感じることが多かったです。<br>
+            お客様のビジネスを持続する為に、資金調達のプロフェッショナルであるアウル経済におまかせいただければ幸いです。</p>
+        </div>
+      </div>
+      <div class="flex">
+        <div><img src="img/staff_08.jpg" alt="代表ワーク風景"></div>
+        <div><img src="img/staff_09.jpg" alt="女性社員ワーク風景"></div>
+      </div>
+    </div>
+  </section>
+  <section id="ceo">
+    <div class="widthKeeper flex">
+      <div class="left">
+        <h3><img src="img/ceo__img_title.svg" alt=""></h3>
+        <p>
+          アウル経済では「社会的責任を全うする」という経営理念を掲げ、
+          常にお客様の視点に立つことを心がけております。<br>
+          お客様一人ひとりの企業価値・企業評価の向上に貢献するべく、
+          専門的知識を活用し、全力で取り組みます。<br><br>
+
+          また私達の最大のミッションは「社会活動へ貢献する」ことです。<br>
+          その一例として、買掛金や人件費の支払い等、資金繰りでお困りのお客様へ積極的なサポートをいたします。<br><br>
+
+          私達は努力を惜しまず、企業の活性化と経済の発展に貢献するため、
+          社員全員の持続的成長を実現いたします。<br>
+        </p>
+      </div>
+      <div class="right">
+        <img src="img/ceo__img_photo.jpg" alt="代表近影">
+        <img src="img/ceo__img_sign.svg" alt="株式会社アウル経済 代表取締役 諸喜田 憲">
+      </div>
+    </div>
+  </section>
+  <section id="faq">
+    <div class="greenTitle">
+      <div class="widthKeeper posR">
+        <div class="transparentGreen">
+        </div>
+        <h2 class="greenBack">よくあるご質問</h2>
+      </div>
+    </div>
+    <div id="faqBox" class="widthKeeper">
+      <ul>
+        <li>
+          <details>
+            <summary>Q.買取最低金額はありますか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.買取上限金額はありますか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.公庫や銀行から借り入れ中ですが、利用できますか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.手数料はどのぐらいですか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.どのような書類が必要ですか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.買取不可能な業種はありますか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.2社間取引ですか？3社間取引ですか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.売掛先が倒産した場合どうなりますか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.連帯保証人は必要ですか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.取引先に知られますか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>Q.赤字ですが利用可能ですか？</summary>
+            <p>原則100万円以上からとなっています。</>
+          </details>
+        </li>
+
+      </ul>
+    </div>
+  </section>
+  <section id="company">
+    <div class="greenTitle">
+      <div class="widthKeeper posR">
+        <div class="transparentGreen">
+        </div>
+        <h2 class="greenBack">運営会社</h2>
+      </div>
+    </div>
+    <div id="companyInfo" class="widthKeeper">
+      <dl class="flex">
+        <dt>会社名</dt>
+        <dd>アウル経済</dd>
+      </dl>
+      <dl class="flex">
+        <dt>代表取締役</dt>
+        <dd>諸喜田 憲</dd>
+      </dl>
+      <dl class="flex">
+        <dt>所在地</dt>
+        <dd>東京都中央区日本橋2-2-3 RISHEビル UCF4階</dd>
+      </dl>
+      <dl class="flex">
+        <dt>設立年月</dt>
+        <dd>平成9年8月</dd>
+      </dl>
+      <dl class="flex">
+        <dt>資本金</dt>
+        <dd>4,850万円</dd>
+      </dl>
+      <dl class="flex">
+        <dt>主要取引銀行</dt>
+        <dd>みずほ銀行</dd>
+      </dl>
+      <dl class="flex">
+        <dt>TEL</dt>
+        <dd>03-6902-1815</dd>
+      </dl>
+      <dl class="flex">
+        <dt>FAX</dt>
+        <dd>03-6902-1812</dd>
+      </dl>
+      <dl class="flex">
+        <dt>Email</dt>
+        <dd>info@owl-economy.jp</dd>
+      </dl>
+    </div>
+  </section>
+  <footer>
+    <div class="widthKeeper flex">
+      <div class="left">
+        <img src="img/logo_white.svg" alt="">
+        <p>アウル経済の請求書買取り</p>
+      </div>
+      <div class="right">
+        <a href="policy.php">プライバシーポリシー</a>
+      </div>
+    </div>
+  </footer>
 </body>
 
 </html>
